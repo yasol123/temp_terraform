6d9f38e754dddd9c6e7180f16d79933c88589325
Yasol Jeong <74160217+yasol123@users.noreply.github.com>
2021-11-11 14:20:29 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "${file("../access_key.txt")}"
-  secret_key = "${file("../secret_key.txt")}"
+#   access_key = "${file("../access_key.txt")}"
+#   secret_key = "${file("../secret_key.txt")}"
 }
